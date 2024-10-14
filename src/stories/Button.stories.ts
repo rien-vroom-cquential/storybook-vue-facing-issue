d49c48672978f8d15b9ea432445b0e56ccd09ed4
Tim Vroom <rien.vroom@c-quential.nl>
2024-10-14 16:17:16 +0200

    Synced code

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,12 +1,12 @@
 import { fn } from '@storybook/test';
-import type { Meta, StoryObj } from '@storybook/vue3';
+import type { Meta, StoryObj, ComponentPropsAndSlots } from '@storybook/vue3';
 
 import Button from './Button.vue';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta = {
   title: 'Example/Button',
-  component: Button,
+  component: Button as ComponentPropsAndSlots<typeof Button>,
   // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   argTypes: {
--- src/stories/Button.vue
+++ src/stories/Button.vue
@@ -3,35 +3,34 @@
 </template>
 
 <script lang="ts">
-
-import {Component, Prop, Vue} from 'vue-facing-decorator';
+import {Component, Prop, Vue } from 'vue-facing-decorator';
 import './button.css';
 import { computed } from 'vue';
 
-@Component()
-    export default class Button extends Vue {
-        @Prop() label!: string;
-        @Prop({type: Boolean}) primary?: boolean;
-        @Prop() size?: 'small' | 'medium' | 'large';
-        @Prop() backgroundColor?: string;
+@Component({})
+export default class Button extends Vue {
+    @Prop() label!: string;
+    @Prop({type: Boolean}) primary?: boolean;
+    @Prop() size?: 'small' | 'medium' | 'large';
+    @Prop() backgroundColor?: string;
 
-        public get classes() {
-            return {
-              'storybook-button': true,
-              'storybook-button--primary': this.primary,
-              'storybook-button--secondary': !this.primary,
-              [`storybook-button--${this.size || 'medium'}`]: true,
-            }
-        }
-
-        public get style() {
-            return {
-              backgroundColor: this.backgroundColor
-            }
-        }
-
-        public onClick() {
-            this.emit('click', 1);
+    public get classes() {
+        return {
+          'storybook-button': true,
+          'storybook-button--primary': this.primary,
+          'storybook-button--secondary': !this.primary,
+          [`storybook-button--${this.size || 'medium'}`]: true,
         }
     }
+
+    public get style() {
+        return {
+          backgroundColor: this.backgroundColor
+        }
+    }
+
+    public onClick() {
+        this.$emit('click', 1);
+    }
+}
 </script>
